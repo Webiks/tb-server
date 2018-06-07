9f09e8d36cb9f5ad7cb19bae3154a43d8d840780
dafnaweiss <dafna@webiks.com>
2018-06-07 19:07:02 +0300

    add the get layer details server

--- api/layers.js
+++ api/layers.js
@@ -3,14 +3,45 @@ const router = express.Router();
 const axios = require('axios');
 const { Authorization } = require('../config/config');
 
-router.get('/', (req, res) => {
-    axios.get('http://localhost:8080/geoserver/rest/workspaces/tb/layers.json', { headers: { Authorization } }).then((response) => {
+router.get('/:worldName', (req, res) => {
+    axios.get(`http://localhost:8080/geoserver/rest/workspaces/${req.params.worldName}/layers.json`, { headers: { Authorization } }).then((response) => {
         res.send(response.data);
+        return response.data;
     }).catch((error) => {
         console.log("error!", error.message);
         res.send('error');
     });
 });
+
+router.get('/:worldName/:layerName', (req, res) => {
+    return getLayerResource (req, res, req.params.worldName, req.params.layerName);
+});
+
+router.get('/:worldName/:layerName/details', (req, res) => {
+    axios.get(`http://localhost:4000/api/layers/${req.params.worldName}/${req.params.layerName}`)
+         .then(res => {
+             // console.log("res data href: " + JSON.stringify(res.data.layer.resource.href));
+             axios.get(res.data.layer.resource.href, { headers: { Authorization } })
+                     .then((response) => {
+                         console.log("get Layer Details: " + JSON.stringify(response.data));
+                         return response.data;
+                    }).catch((error) => {
+                         console.log("error!", error.message);
+                         res.send('error');
+                    });
+         });
+});
+
+function getLayerResource (req, res, worldName, layerName){
+    axios.get(`http://localhost:8080/geoserver/rest/workspaces/${worldName}/layers/${layerName}.json`, { headers: { Authorization } })
+        .then((response) => {
+            res.send(response.data);
+            return response.data;
+        }).catch((error) => {
+            console.log("error!", error.message);
+            res.send('error');
+        });
+}
 
 module.exports = router;
 
