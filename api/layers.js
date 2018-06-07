71ed598487a47a9b32029ae67e43d830d7c9cbe6
dafnaweiss <dafna@webiks.com>
2018-06-07 19:36:02 +0300

    finish the get layer details server

--- api/layers.js
+++ api/layers.js
@@ -4,32 +4,32 @@ const axios = require('axios');
 const { Authorization } = require('../config/config');
 
 router.get('/:worldName', (req, res) => {
-    axios.get(`http://localhost:8080/geoserver/rest/workspaces/${req.params.worldName}/layers.json`, { headers: { Authorization } }).then((response) => {
-        res.send(response.data);
-        return response.data;
-    }).catch((error) => {
-        console.log("error!", error.message);
-        res.send('error');
-    });
+    axios.get(`http://localhost:8080/geoserver/rest/workspaces/${req.params.worldName}/layers.json`, { headers: { Authorization } })
+        .then((response) => {
+            res.send(response.data);
+            return response.data;
+        })
+        .catch((error) => {
+            console.log("error!", error.message);
+            res.send('error');
+        });
 });
 
-router.get('/:worldName/:layerName', (req, res) => {
-    return getLayerResource (req, res, req.params.worldName, req.params.layerName);
-});
+router.get('/:worldName/:layerName', (req, res) => getLayerResource (req, res, req.params.worldName, req.params.layerName));
 
 router.get('/:worldName/:layerName/details', (req, res) => {
     axios.get(`http://localhost:4000/api/layers/${req.params.worldName}/${req.params.layerName}`)
-         .then(res => {
-             // console.log("res data href: " + JSON.stringify(res.data.layer.resource.href));
-             axios.get(res.data.layer.resource.href, { headers: { Authorization } })
-                     .then((response) => {
-                         console.log("get Layer Details: " + JSON.stringify(response.data));
-                         return response.data;
-                    }).catch((error) => {
-                         console.log("error!", error.message);
-                         res.send('error');
-                    });
+         .then(res => axios.get(res.data.layer.resource.href, { headers: { Authorization } }))
+         .then((response) => {
+             console.log("get Layer Details: " + JSON.stringify(response.data));
+             res.send(response.data);
+             return response.data;
+         })
+         .catch((error) => {
+             console.log("error!", error.message);
+             res.send('error');
          });
+
 });
 
 function getLayerResource (req, res, worldName, layerName){
@@ -37,7 +37,8 @@ function getLayerResource (req, res, worldName, layerName){
         .then((response) => {
             res.send(response.data);
             return response.data;
-        }).catch((error) => {
+        })
+        .catch((error) => {
             console.log("error!", error.message);
             res.send('error');
         });
