352106197c40f9b38fddec95b28a94449af622d9
dafnaweiss <37504470+dafnaweiss@users.noreply.github.com>
2018-06-12 11:54:38 +0300

    add the Layer's details request to the server (#12)
    * add the get layer details server
    
    * finish the get layer details server
    
    * get worlds, get layers, get layer's details

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ const express = require('express');
 const cors = require('cors');
 const api = require('./api');
 const config = require('./config/config');
+
 const app = express();
 
 app.use(cors());
 app.use('/api', api);
 
-app.listen(config.port, () => console.log('listen to ', config.port));
+app.listen(config.serverPort, () => console.log('listen to ', config.serverPort));
